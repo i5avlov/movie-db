0c926e845e6fa18140d73c613f4a57bfa808c3ec
i5avlov <ipavlov@abv.bg>
2026-03-13 13:31:03 +0200

    Layered structure defined for project

---
+++ src/MovieDb/MovieDb.Infrastructure/Data/MovieDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieDb.Infrastructure.Data
+{
+    public class MovieDbContext : IdentityDbContext
+    {
+        public MovieDbContext(DbContextOptions<MovieDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
